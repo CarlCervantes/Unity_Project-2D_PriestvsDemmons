1ef21e174fe91da9d871102e665e9b7809d4681a
CarlCervantes <61489477+CarlCervantes@users.noreply.github.com>
2022-03-04 12:19:44 -0600

    Correccion de botones

---
+++ Assets/Scripts/EventMENU.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class EventMENU : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play_Replay()
+    {
+        SceneManager.LoadScene("Game_Beta");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controles");
+    }
+    public void Return()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+    public void closeGame()
+    {
+        Application.Quit();
+    }
+}
